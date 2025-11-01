ff49b0d0825b1509f69a46aa72c68acc82ffae72
Cavan Ray Theiss <cavanrt@gmail.com>
2025-11-01 18:07:06 -0500

    adding to to-do list

--- Daily_Log_2025.cpp
+++ Daily_Log_2025.cpp
@@ -8,6 +8,7 @@ updates this morning went smoothly, had a rough patch trying to get home but I m
 
 Gonna try and get a few things done:
     - do laundry
+        - start bedding
 	- wash dishes
 	- sweep house
 	- write some code
